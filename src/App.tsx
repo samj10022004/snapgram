82715be2edf591754e08864a4bcfcbb02961004c
samj10022004 <97349711+samj10022004@users.noreply.github.com>
2024-04-30 23:50:05 +0530

    notfoundpage

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import SignupForm from './_auth/forms/SignupForm';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
 import { Toaster } from "@/components/ui/toaster"
+import NotFoundPage from './NotFoundPage';
 const App = () => {
   return (
     <main className='flex h-screen'>
